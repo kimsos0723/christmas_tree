5105f4a0f3d06156eedaee2ea0435877da26244b
KimSeongWook <kimsos0723@gmail.com>
2020-12-18 19:06:56 +0900

    sum

--- Python/ascii_art_tree.py
+++ Python/ascii_art_tree.py
@@ -1,7 +1,7 @@
 import os
 import numpy as np
 from PIL import Image
-from fractions import Fraction
+
 # image_path = input()
 image_path = "tree.gif"
 common_dividors = []
@@ -22,14 +22,26 @@ with Image.open(image_path) as img:
     if img.is_animated:
         pass
     else:
-        rgb_img = img.convert('RGBA')
+        rgb_img = img.convert('RGB')
         pixels = rgb_img.load()
         n = common_dividor(xsize, ysize)
         factor = n[len(n) // 2]
-        print(xsize, ysize)
-        print(Fraction(xsize, ysize))
-        print(n, factor, np.gcd(xsize, ysize))
-
+        ascii_pixel_matrix = []
+        for y in range(0, ysize, factor):
+            ascii_pixel_row=[]
+            for x in range(0, xsize, factor):
+                pixel_color_sum = [0,0,0]
+                for i in range(factor):
+                    for j in range(factor):
+                        pixel_color_sum[0] += rgb_img.getpixel((x+j,y+i))[0]
+                        pixel_color_sum[1] += rgb_img.getpixel((x+j,y+i))[1]
+                        pixel_color_sum[2] += rgb_img.getpixel((x+j,y+i))[2]
+                        
+                ascii_pixel_row.append(pixel_color_sum)
+            ascii_pixel_matrix.append(ascii_pixel_row)
+        
+        print(ascii_pixel_matrix)
+        
         rgb_img.save('tree2.png')
 
    
